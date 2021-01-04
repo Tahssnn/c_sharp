93ff46210ad760a088fbf8a667561ee389c1f6cd
Tahsin ŞENER <50183953+Tahssnn@users.noreply.github.com>
2021-01-04 19:06:24 +0300

    C#

---
+++ Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //1
+            string[] students = new string[3];  // 3 elemanlı students adında bir array tanımlıyoruz.
+            students[0] = "Engin";  // students arrayınin 0. elemanı
+            students[1] = "Derin";
+            students[2] = "Salih";
+            //2
+            string[] students2 = new[] { "Engin", "Derin", "Salih" };       //aynı şey 2. tanımlama şekli daha pratik
+            //3
+            string[] students3 = { "Engin", "Derin", "Salih" };             //new demeden de yazabilirdik
+
+            foreach (var student in students2)
+            {
+                Console.WriteLine(student);
+            }
+            //-------------------------------4-Çok boyulu dizi-------------------------------------------------
+
+            string[,] regions = new string[4, 3]      // 4 satır 3 kolunlu
+            {
+                {"İstanbul","İzmit","Balıkesir" },
+                {"Antalya","Adana","Mersin" },
+                {"Samsun","Rize","Sinop" },
+                {"İzmir","Muğla","Manisa" }
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i, j]);
+
+                }
+                Console.WriteLine("**********************");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ForLoop();
+            //WhileLoop();
+            //DoWhileLoop();
+            //ForEachLoop();
+
+            Console.ReadLine();
+        }
+
+        private static void ForEachLoop()   //dizi temelli
+        {
+            string[] students = new string[3] { "Engin", "Derin", "Salih" };
+            foreach (var student in students)
+            {
+                Console.WriteLine(students);
+            }
+        }
+
+        private static void DoWhileLoop()
+        {
+            int number = 10;
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+            } while (number >= 0);
+        }
+
+        private static void WhileLoop()
+        {
+            int number = 100;
+            while (number >= 0)
+            {
+                Console.WriteLine(number);
+                number--;   // 1er azalt.
+            }
+        }
+
+        private static void ForLoop()
+        {
+            for (int i = 10; i <= 100; i += 10)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
